c4dc0904c32abb125a7a8046a6457e4be0d4c4ad
Alexander Gureshidze <alexander@growmo.re>
2022-04-29 12:36:18 +0400

    Each speaker files merged into one file

--- cut_audio_into_pieces.py
+++ cut_audio_into_pieces.py
@@ -9,12 +9,13 @@ g_base_dir = sys.argv[1]
 original_audio_file = sys.argv[2]
 xml_file = sys.argv[3]
 
+dest_mp3s = f'{g_base_dir}/mp3s'
 sound = AudioSegment.from_file(os.path.join(g_base_dir, f"{original_audio_file}.mp3"))
 
 def take_portion_from_audio(first_cut_point, last_cut_point, portion_name, timestamp) :
     sound_clip = sound[first_cut_point:last_cut_point]
     portion_name = portion_name.replace(' ', '_')
-    dest_path = f'{g_base_dir}/mp3s/{portion_name}'
+    dest_path = f'{dest_mp3s}/{portion_name}'
     Path(dest_path).mkdir(parents=True, exist_ok=True)
     sound_clip.export(os.path.join(dest_path, f"{timestamp}_{first_cut_point}_{last_cut_point}.mp3"), format="mp3")
 
@@ -45,3 +46,28 @@ for i in range(len(records)):
     else :
         #This is final line
         print('this', records[i])
+
+# Get sorted speakers dirs
+speakers_dirs = []
+for dir in os.scandir(dest_mp3s):
+    if dir.is_dir():
+        speakers_dirs.append(dir.name)
+speakers_dirs.sort()
+
+#Iterate each speaker dir
+for dir in speakers_dirs :
+    person_dir = f"{dest_mp3s}/{dir}"
+    print(person_dir)
+    for _, _, f_list in os.walk(person_dir):
+        combined_file = AudioSegment.empty()
+        #Iterate each mp3 file for this speaker
+        # and combine all of them into one final file.
+        # Final filename will be conbined.mp3
+        for file in f_list:
+            print(f'    {file}')
+            try:
+                audio_segment = AudioSegment.from_file(f"{person_dir}/{file}", format="mp3")
+                combined_file += audio_segment
+            except:
+                print(f'        {file} failed to combine')
+        combined_file.export(f"{person_dir}/combined.mp3", format="mp3")
